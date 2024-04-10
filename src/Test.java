b8c1fd94bf41dd20d40aa23512e3f500fbf9d9aa
BieKub <149608986+BieKub@users.noreply.github.com>
2024-04-10 13:40:04 -0400

    Added time

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,15 @@
 public class Test {
     public static void main(String[] args) throws Exception {
 
+    System.currentTimeMillis();
+
+    long startTime = System.nanoTime();
+
+    long endTime = System.nanoTime();
+
+    long timeElapsed = (endTime - startTime);
+    
+    System.out.println("Processing Time: " + timeElapsed + "ns");
 
 
 
