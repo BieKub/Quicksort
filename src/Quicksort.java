1e2cce3b00449825c06563c7d2ae6f4c70c32185
BieKub <149608986+BieKub@users.noreply.github.com>
2024-04-10 13:19:52 -0400

    Added sort and print methods

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,59 +1,19 @@
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.BufferedReader;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        String path = "C:\\Users\\kubni\\OneDrive\\Desktop\\Coding\\Test\\Data21.txt";
-
-        FileReader fileReader = new FileReader(path);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-
-        String line;
-        while ((line = bufferedReader.readLine()) != null)
-        {
-            arr.add(Integer.parseInt(line));
-        }
-
-        bufferedReader.close();
-
-
-
-        for (int i = 0; i < arr.size(); i++)
-        {
-            int sum = 0;
-            boolean check = false;
-
-            if (arr.get(i) / 1000 >= 5)
-            {
-                sum += arr.get(i) / 1000;
-                sum += (arr.get(i) / 100) % 10;
-                sum += (arr.get(i) / 10) % 10;
-                sum += arr.get(i) % 10;
-                if (sum < 18)
-                {
-                    check = true;
-                }
-            }
-
-            if (check)
-            {
-                System.out.println("So cute!");  
-            }
-            else 
-            {
-                System.out.println("Sorry");
-            }
-        }
-
-        
-        
-        
-        
-
-
+public class Quicksort {
+    
+    public static void swap(int[] arr, int i, int j)
+    {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
     }
+
+    public static void printArray(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
